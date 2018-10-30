1a0d9bf35fcdf1b326407054769667a130e4702c
iai3 <34685015+iai3@users.noreply.github.com>
2018-10-30 21:30:30 +0100

    removed mock

--- src/app.js
+++ src/app.js
@@ -12,20 +12,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'water bill', amount: 4500, createdAt: 1000}))
-store.dispatch(addExpense({description: 'gas bill', amount: 2000, createdAt: -1000}))
-store.dispatch(addExpense({description: 'rent', amount: 50000, createdAt: 500}))
-// store.dispatch(setTextFilter('water'))
-
-// setTimeout(() => {
-//     store.dispatch(setTextFilter('gas'))
-// }, 2000);
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
-console.log(visibleExpenses);
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
